e2646e29876eb018c4bb18547e0e8854c01dc88f
Hong-Jun Eom <nara1030@naver.com>
2020-05-21 01:56:33 +0900

    Create Ex_02.java

---
+++ src/ch02/Ex_02.java
@@ -0,0 +1,42 @@
+package ch02;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ex_02 {
+    final List<String> uppercaseNames = new ArrayList<>();
+
+    public static void main(String[] args) {
+        Ex_02 ex_02 = new Ex_02();
+
+        for (String name : Friends.friends) {
+            ex_02.uppercaseNames.add(name.toUpperCase());
+        }
+        System.out.println(ex_02.uppercaseNames);
+        ex_02.uppercaseNames.clear();
+        System.out.println("리스트 비어있는지?: " + ex_02.uppercaseNames.isEmpty());
+
+        System.out.println("=====");
+
+        Friends.friends.forEach(name -> ex_02.uppercaseNames.add(name.toUpperCase()));
+        System.out.println(ex_02.uppercaseNames);
+        ex_02.uppercaseNames.clear();
+
+        System.out.println("=====");
+        Friends.friends.stream()
+                .map(name -> name.toUpperCase())
+                .forEach(name -> System.out.print(name + ", "));
+
+        System.out.println();
+        System.out.println("=====");
+        Friends.friends.stream()
+                .map(name -> name.length())
+                .forEach(count -> System.out.print(count + ", "));
+
+        System.out.println();
+        System.out.println("=====");
+        Friends.friends.stream()
+                .map(String::toUpperCase)
+                .forEach(name -> System.out.print(name + ", "));
+    }
+}
